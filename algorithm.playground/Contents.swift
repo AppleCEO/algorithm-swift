3767d0c1e718b3978d7a99e7884cc45c975c35bd
도미닉 <kiljh@me.com>
2019-11-20 16:33:32 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12925 문제 풀이

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,1 +1,11 @@
+func solution(_ s:String) -> Int {
+    if s[s.startIndex] == "-" {
+        return -1*(Int(String(s[s.index(after: s.startIndex)..<s.endIndex])) ?? 0)
+    }
+    
+    return Int(String(s[s.startIndex..<s.endIndex])) ?? 0
+}
 
+solution("+1")
+solution("-1")
+solution("1")
