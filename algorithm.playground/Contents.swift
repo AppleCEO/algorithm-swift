415dba87a914d3b14751c4414bd9f20a32ea959a
도미닉 <kiljh@me.com>
2019-11-21 23:37:06 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12933 문제 풀이

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,16 +1,25 @@
 import Foundation
 
-func solution(_ n:Int64) -> [Int]
+func solution(_ n:Int64) -> Int
 {
-    var answer:[Int] = []
+    var array:[Int] = []
+    var result = ""
     
     for number in String(n)
     {
-        answer.insert(Int(String(number)) ?? 0, at: 0)
+        array.insert(Int(String(number)) ?? 0, at: 0)
     }
     
-    return answer
+    array.sort { (left, right) -> Bool in
+        return left > right
+    }
+    
+    array.forEach { (number) in
+        result += String(number)
+    }
+    
+    return Int(result) ?? 0
 }
 
-solution(123)
-solution(987)
+solution(118372)
+
