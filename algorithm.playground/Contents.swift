8732e9242bc848f834a75a319dc93aae07cbdfbd
도미닉 <kiljh@me.com>
2019-11-22 17:46:11 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12940 문제 풀이

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,6 +1,21 @@
-func solution(_ num:Int) -> String {
-    return num%2 == 0 ? "Even" : "Odd"
+func solution(_ n:Int, _ m:Int) -> [Int] {
+    let gcd: Int
+    let lcm: Int
+
+    func calculateGCD(_ n: Int, _ m: Int) -> Int {
+        if m == 0 {
+            return n
+        }
+            
+        return calculateGCD(m, n%m)
+    }
+    
+    gcd = calculateGCD(n, m)
+    lcm = n * m / gcd
+    
+    return [gcd, lcm]
 }
 
-solution(3)
-solution(4)
+solution(3, 12)
+solution(2, 5)
+solution(11, 144)
