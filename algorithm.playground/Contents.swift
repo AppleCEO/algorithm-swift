c00a85d8456722f80fbaae6ded258e4cf623216a
도미닉 <kiljh@me.com>
2019-11-26 00:39:01 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12954 문제 풀이

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,19 +1,13 @@
-func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
-    var i = 0, j = 0
+func solution(_ x:Int, _ n:Int) -> [Int64] {
+    let result = Array<Int>.init(repeating: 0, count: n)
+    var index = 0
     
-    return arr1.map { (numbers) -> [Int] in
-        defer {
-            i += 1
-            j = 0
-        }
-        return numbers.map { (number) -> Int in
-            defer {
-                j += 1
-            }
-            return number + arr2[i][j]
-        }
+    return result.map { (_) -> Int64 in
+        index += 1
+        return Int64(index*x)
     }
 }
 
-solution([[1,2],[2,3]], [[3,4],[5,6]])
-solution([[1],[2]], [[3],[4]])
+solution(2, 5)
+solution(4, 3)
+solution(-4, 2)
