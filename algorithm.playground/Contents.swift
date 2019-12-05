0c8d5f863e82d82728e39cd6046875315a952370
unknown <kiljh@me.com>
2019-12-05 18:04:45 +0900

    https://app.codility.com/programmers/lessons/4-counting_elements/perm_check/ 문제 풀이

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,26 +1,11 @@
 public func solution(_ A : inout [Int]) -> Int {
-    var lhs = A.first!
-    var rhs = A.reduce(0, { (s1: Int, s2: Int) -> Int in
-        return s1 + s2
-    }) - lhs
-    var minimalDifference = lhs - rhs
-    if minimalDifference < 0 {
-        minimalDifference *= -1
-    }
+    let sortedA = A.sorted()
     
-    for index in 1..<A.count-1 {
-        lhs += A[index]
-        rhs -= A[index]
-        
-        var difference = lhs - rhs
-        if difference < 0 {
-            difference *= -1
-        }
-        
-        if difference < minimalDifference {
-            minimalDifference = difference
+    for (index, item) in sortedA.enumerated() {
+        if index + 1 != item {
+            return 0
         }
     }
     
-    return minimalDifference
+    return 1
 }
