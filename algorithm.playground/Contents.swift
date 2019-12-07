1623de34284d545d13cde9172350241db144bd53
도미닉 <kiljh@me.com>
2019-12-07 22:58:15 +0900

    https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/ 문제 풀이

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,38 +1,21 @@
-public func solution(_ N : Int, _ A : inout [Int]) -> [Int] {
+public func solution(_ A : inout [Int]) -> Int {
     var dictionary = [Int: Int]()
-    var max = 0
-    var valueMax = 0
-    var answer = [Int]()
     
     for item in A {
-        if item == N+1 {
-            max = valueMax
-            continue
-        }
-        
-        if let value = dictionary[item-1], value < max {
-            dictionary[item-1] = max + 1
-        } else {
-            dictionary[item-1] = (dictionary[item-1] ?? max) + 1
-        }
-        
-        if let value = dictionary[item-1], value > valueMax {
-            valueMax = value
+        if item <= A.count && item > 0 {
+            dictionary[item] = (dictionary[item] ?? 0) + 1
         }
     }
     
-    for index in 0..<N {
-        if let value = dictionary[index] {
-            if value < max {
-                answer.append(max)
-                continue
-            }
-            answer.append(value)
-            continue
-        } else {
-            answer.append(max)
+    if dictionary.count == 0 {
+        return 1
+    }
+    
+    for index in 1...dictionary.count {
+        if dictionary[index] == nil {
+            return index
         }
     }
     
-    return answer
+    return dictionary.count+1
 }
