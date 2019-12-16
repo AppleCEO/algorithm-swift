60e4f97313b33c70397258c1086d3cd8f4a1e37f
도미닉 <kiljh@me.com>
2019-12-16 20:06:08 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42588 문제 풀이

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,42 +1,19 @@
 import Foundation
 
-func solution(_ priorities:[Int], _ location:Int) -> Int {
-    var index = 0
-    var array = priorities.map { (number) -> (Int, Int) in
-        index += 1
-        return (index, number)
-    }
+func solution(_ heights:[Int]) -> [Int] {
+    var answer = [Int].init(repeating: 0, count: heights.count)
     
-    var count = priorities.count
-    var order = [Int]()
-    index = 0
-
-    while order.count < priorities.count {
-        defer {
-            if index >= array.count {
-                index = 0
+    for (index, height) in heights.enumerated() {
+        for innerIndex in 0...index {
+            let topIndex = index-innerIndex
+            if heights[topIndex] > height {
+                answer[index] = topIndex+1
+                break
             }
         }
-        let max = array.max(by: { (left, right) -> Bool in
-            left.1 < right.1
-        })!
-        if array[index].1 == max.1 {
-            order.append(array[index].0)
-            array.remove(at: index)
-            continue
-        }
-
-        index += 1
-        
-    }
-
-    for index in 0..<order.count {
-        if order[index] == location+1 {
-            return index+1
-        }
     }
     
-    return 0
+    return answer
 }
 
-solution([1, 1, 9, 1, 1, 1], 0)
+solution([6,9,5,7,4])
