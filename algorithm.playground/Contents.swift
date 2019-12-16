acbfdb6bcd744461b67310b399366396b969d733
도미닉 <kiljh@me.com>
2019-12-16 17:09:43 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42585 문제 풀이

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,1 +1,28 @@
+import Foundation
 
+func solution(_ arrangement:String) -> Int {
+    var ironRodCount = 0
+    var answer = 0
+    let array = Array(arrangement)
+    
+    for (index, character) in arrangement.enumerated() {
+        if character == "(" {
+            ironRodCount += 1
+            continue
+        }
+        
+        if character == ")" {
+            ironRodCount -= 1
+            if array[index-1] == ")" {
+                answer += 1
+                continue
+            }
+            
+            answer += ironRodCount
+        }
+    }
+    
+    return answer
+}
+
+solution("()(((()())(())()))(())")
