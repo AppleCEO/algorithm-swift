7b9518f8708fa60c1f56dcf85c1b04bc96b949b4
도미닉 <kiljh@me.com>
2019-12-16 19:14:19 +0900

    https://programmers.co.kr/learn/courses/30/lessons/42587 문제 풀이

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -1,28 +1,42 @@
 import Foundation
 
-func solution(_ arrangement:String) -> Int {
-    var ironRodCount = 0
-    var answer = 0
-    let array = Array(arrangement)
+func solution(_ priorities:[Int], _ location:Int) -> Int {
+    var index = 0
+    var array = priorities.map { (number) -> (Int, Int) in
+        index += 1
+        return (index, number)
+    }
     
-    for (index, character) in arrangement.enumerated() {
-        if character == "(" {
-            ironRodCount += 1
+    var count = priorities.count
+    var order = [Int]()
+    index = 0
+
+    while order.count < priorities.count {
+        defer {
+            if index >= array.count {
+                index = 0
+            }
+        }
+        let max = array.max(by: { (left, right) -> Bool in
+            left.1 < right.1
+        })!
+        if array[index].1 == max.1 {
+            order.append(array[index].0)
+            array.remove(at: index)
             continue
         }
+
+        index += 1
         
-        if character == ")" {
-            ironRodCount -= 1
-            if array[index-1] == ")" {
-                answer += 1
-                continue
-            }
-            
-            answer += ironRodCount
+    }
+
+    for index in 0..<order.count {
+        if order[index] == location+1 {
+            return index+1
         }
     }
     
-    return answer
+    return 0
 }
 
-solution("()(((()())(())()))(())")
+solution([1, 1, 9, 1, 1, 1], 0)
