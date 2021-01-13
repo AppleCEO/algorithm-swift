7d13d61dd817f87f4f533ec2245f83558d39664e
도미닉 <kiljh@me.com>
2021-01-14 08:53:06 +0900

    Create 1001.swift

---
+++ baekjoon/1001.swift
@@ -0,0 +1,9 @@
+let input = readLine()
+let splited = input!.split(separator: " ")
+let numbers = splited.map({ (string) -> Int in
+    return (Int(string) ?? 0)
+})
+let result = numbers.reduce(0, { (first:Int, second: Int) -> Int in
+    return first - second
+})
+print(result)
