6516549a470077a5c83eded12923d2b23ca4bcba
도미닉 <kiljh@me.com>
2021-01-14 09:36:53 +0900

    Update 1001.swift

--- baekjoon/1001.swift
+++ baekjoon/1001.swift
@@ -3,7 +3,5 @@ let splited = input!.split(separator: " ")
 let numbers = splited.map({ (string) -> Int in
     return (Int(string) ?? 0)
 })
-let result = numbers.reduce(0, { (first:Int, second: Int) -> Int in
-    return first - second
-})
+let result = numbers[0] - numbers[1]
 print(result)
